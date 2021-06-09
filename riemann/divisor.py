6fc260edd01429c01b9f84d520853377ad94cb0e
Isaac Owomugisha <archimedesisaac@gmail.com>
2021-06-09 19:03:49 +0300

    Add faster implementation of divisor sum (faster with the help of numba)

--- riemann/divisor.py
+++ riemann/divisor.py
@@ -1,7 +1,22 @@
 '''Compute the sum of divisors of a number.'''
+from functools import lru_cache
+from numba import njit
+import math
 
+
+@njit
 def divisor_sum(n: int) -> int:
+    '''Compute the sum of divisors of a positive integer.'''
     if n <= 0:
-        raise ValueError("Non-positive numbers are not supportedd.")
+        return 0
     
-    return sum(i for i in range(1, n + 1) if n % i == 0)
+    i = 1
+    limit = math.sqrt(n)
+    the_sum = 0
+    while i <= limit:
+        if n % i == 0:
+            the_sum += i
+            if i != limit:
+                the_sum += n // i
+        i += 1
+    return the_sum
