32d1fb2651901ba93abeab42763ef3ac515da892
Isaac Owomugisha <archimedesisaac@gmail.com>
2021-06-15 18:01:40 +0300

    switch to sqlite

--- riemann/populate_database.py
+++ riemann/populate_database.py
@@ -2,14 +2,18 @@
 import time
 from riemann.database import DivisorDb
 from riemann.divisor import compute_riemann_divisor_sums
-from riemann.in_memory_database import InMemoryDivisorDb
+from riemann.sqlite_database import SqliteDivisorDb
 
 
-def populate_db(db: DivisorDb, batch_size: int = 100000) -> None:
+def populate_db(db: DivisorDb, batch_size: int = 250000) -> None:
     '''Populate the db in batches.
     Write the computed divisor sums to the database after each batch.
     '''
-    starting_n = (db.summarize().largest_computed_n or 5040) + 1
+    largest_n = db.summarize().largest_computed_n 
+    if largest_n is None:
+        starting_n = 5041
+    else:
+        starting_n = largest_n.n + 1
     while True:
         ending_n = starting_n + batch_size
         start = time.time()
@@ -20,7 +24,8 @@ def populate_db(db: DivisorDb, batch_size: int = 100000) -> None:
 
 
 if __name__ == "__main__":
-    db = InMemoryDivisorDb()
+    import sys
+    db = SqliteDivisorDb(database_path=sys.argv[1])
     try:
         populate_db(db)
     except KeyboardInterrupt:
